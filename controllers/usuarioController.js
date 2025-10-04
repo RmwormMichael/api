f1a4ca6d57e282f2f5fdc094ce89d72097c4c335
Your Name <you@example.com>
2025-10-04 15:45:57 -0500

    ya sirve en render

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -19,7 +19,7 @@ const registrar = async (req, res) => {
             nombre,
             email,
             password,
-            rol: "cliente",
+            rol: "admin",
             token: generarId(),  // Generar un token para la confirmación de cuenta
         };
 
